934ce16830adb9abd5b7e0f606a44f49c6932bfc
Maestro <fernandezfrancoa2001@gmail.com>
2021-08-04 12:04:35 -0300

    ahora si

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const api ={
     key: '9e122cd782b2d0333f5fe4e7fa192062',
-    url: 'http://api.openweathermap.org/data/2.5/weather'
+    url: 'https://api.openweathermap.org/data/2.5/weather'
 };
 
 const card = document.querySelector("#card");
